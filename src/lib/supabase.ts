c31c3a37e5f9a013cdf9f1846c2e769acc186d7f
Annelise <annelise@Brandons-MacBook-Pro.local>
2026-07-25 05:53:19 -0500

    API

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://aoglolvcgjheqjxcrvxp.supabase.co";
 
-const supabaseAnonKey = "YOUR_KEY_HERE";
+const supabaseAnonKey = "sb_publishable_KrDUCafyn0_ZbLSW_iPd1g_030I8Po6";
 
 export const supabase = createClient(
   supabaseUrl,
